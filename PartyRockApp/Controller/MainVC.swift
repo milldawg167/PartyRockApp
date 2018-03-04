358dae480ce0bee0c1e97cc8f4c40b44b3a83832
Andrew Miller <milldawg@github.com>
2018-03-04 01:16:58 +0000

    Started initial UI configuration

---
+++ PartyRockApp/Controller/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Andrew Miller on 11/02/2018.
+//  Copyright © 2018 Andrew Miller. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
